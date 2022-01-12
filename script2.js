d26db79257d52f03714db559a1a8ebd703b40260
YujiAsano1 <96355109+YujiAsano1@users.noreply.github.com>
2022-01-12 14:02:00 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
 function comparaNumeros(num1, num2) {
-    if (!num1 || !num2) return "Defina Os Dois Números Ou O Hulk Virá Na Sua Casa!"
+    if (!num1 || !num2) return "Defina Os Dois Números!"
 
     const primeiraFrase = criaPrimeiraFrase(num1, num2);
     const segundaFrase = criaSegundaFrase(num1, num2);
