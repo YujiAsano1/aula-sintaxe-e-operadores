719a29443583303b4c8a07dd869a3cf9e425dd14
Yuji <yujiasano1@gmail.com>
2022-01-12 14:00:55 -0300

    Aula Sintaxe, Talvez Eu Coloque Anotações Depois

---
+++ script.js
@@ -0,0 +1,15 @@
+//algumas anotações.
+
+/*function comparaNumeros(num1, num2) {
+    const saoIguais = num1 === num2;
+    const soma = num1 + num2;
+
+    if(saoIguais) {
+        return "São Iguais";
+    }
+
+    return "Não São Iguais";
+}
+
+return saoIguais ? "São Iguais" : "Não São Iguais";
+*/
